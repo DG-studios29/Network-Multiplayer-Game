11893bd80d8cceef49d109fe0dd47d3dc47f9809
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2025-05-02 21:56:48 +0200

    Create PresetsBtn.cs

---
+++ Network Multiplayer Game/Assets/Scripts/Cannons/PresetsBtn.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PresetsBtn : SelectableBtn
+{
+    [SerializeField] private Image backgroundColour;
+    [SerializeField]private PresetsHolder presetHolder;
+    private bool isHighlighted = false;
+
+
+    private void Awake()
+    {
+        presetHolder = GetComponentInParent<PresetsHolder>();
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+        //not necessary
+        HighlightOff();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void HighlightActive()
+    {
+        if (btnSelected)
+        {
+            backgroundColour.enabled = true;
+            isHighlighted = true;
+            
+        }
+
+
+        if(presetHolder == null)
+        {
+            presetHolder = GetComponentInParent<PresetsHolder>();
+        }
+       
+
+
+        //turn off every other, except this button
+        presetHolder.HighlightOne(this);
+    }
+
+    public void HighlightOff()
+    {
+        backgroundColour.enabled=false;
+        isHighlighted = false;
+        
+    }
+
+    public bool GetHighlightStatus()
+    {
+
+
+        if (presetHolder == null)
+        {
+            presetHolder = GetComponentInParent<PresetsHolder>();
+        }
+
+
+        return isHighlighted;
+    }
+
+}
