ea0a0a5cab4d1b3c12dcf9ceafe36a942d15c207
Dhillan Gopal <70883550+DG-studios29@users.noreply.github.com>
2025-05-03 21:44:49 +0200

    Water update with movement

--- Network Multiplayer Game/Assets/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
+++ Network Multiplayer Game/Assets/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
@@ -13,8 +13,6 @@ namespace Bitgem.VFX.StylisedWater
         #region Public fields
 
         public WaterVolumeHelper WaterVolumeHelper = null;
-        public float splashStrength = 0.5f;
-        private Vector3 lastPos;
 
         #endregion
 
@@ -23,29 +21,12 @@ namespace Bitgem.VFX.StylisedWater
         void Update()
         {
             var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
-            if (!instance) return;
-
-            // Apply floating
-            float? height = instance.GetHeight(transform.position);
-            if (height.HasValue)
+            if (!instance)
             {
-                transform.position = new Vector3(transform.position.x, height.Value, transform.position.z);
+                return;
             }
 
-            // Basic water disturbance prototype
-            if ((transform.position - lastPos).magnitude > 0.1f)
-            {
-                CreateRipple(transform.position, splashStrength);
-            }
-
-            lastPos = transform.position;
-        }
-
-        void CreateRipple(Vector3 pos, float strength)
-        {
-            // Custom function to inject ripple/wave - could modify shader param, buffer, etc.
-            Shader.SetGlobalVector("_RipplePosition", pos);
-            Shader.SetGlobalFloat("_RippleStrength", strength);
+            transform.position = new Vector3(transform.position.x, instance.GetHeight(transform.position) ?? transform.position.y, transform.position.z);
         }
 
         #endregion
