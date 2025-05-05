d996c9bae4472e4b1f4902c46a911a740a4c261e
IDigArtGames <132227781+IdigArtGames@users.noreply.github.com>
2025-05-05 12:56:46 +0200

    fixes

--- Network Multiplayer Game/Assets/Scripts/Wandile_Classes/Player/Boat_Controller.cs
+++ Network Multiplayer Game/Assets/Scripts/Wandile_Classes/Player/Boat_Controller.cs
@@ -99,7 +99,7 @@ public class Boat_Controller : MonoBehaviour
 
         float sqrSpeed = Mathf.Max(rb.linearVelocity.sqrMagnitude, 0.0001f);
         float clampedSqrSpeed = Mathf.Clamp01(sqrSpeed);
-        waterSFx.volume = Mathf.Lerp(waterSFx.volume, clampedSqrSpeed * 0.5f, Time.deltaTime);
+        waterSFx.volume = Mathf.Lerp(waterSFx.volume, clampedSqrSpeed * 0.2f, Time.deltaTime);
 
         float shipDir = Vector3.Dot(shipVel, forward);
 
