dcb428e38d84c14edc978a124c0a8d973a13c44b
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2025-05-06 21:19:04 +0200

    leaderboard attempts

--- Network Multiplayer Game/Assets/Scripts/TempNetwork/NetworkManagerTemp.cs
+++ Network Multiplayer Game/Assets/Scripts/TempNetwork/NetworkManagerTemp.cs
@@ -5,7 +5,18 @@ using System.Collections.Generic;
 public class NetworkManagerTemp : NetworkManager
 {
     //public List<string> player_Names = new List<string>();
-    public string player_Name;
+    //public string player_Name;
+    //private GameObject lbInstance;
+    //[SerializeField]public GameObject LBPrefab;
+
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        //lbInstance = GameObject.Instantiate(LBPrefab);
+
+    }
+
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
@@ -24,20 +35,24 @@ public class NetworkManagerTemp : NetworkManager
         GameObject player = conn.identity.gameObject; //get the player game object
        // from its ID
         PlayerInputTemp playerCTRL = player.GetComponent<PlayerInputTemp>(); //get the exmaple script
-        playerCTRL.SetPlayerName(conn,player_Name,numPlayers);
-
+       
+        
         //on the player prefab
         if (numPlayers == 1) //the very first player to join the game, numPlayers
                            
         {
             // First player (usually the host)
-             playerCTRL.TargetShowWelcomeMessage(conn, "You are the host! Lets hope you dont have hacks to find that treasure.");
-         }
+            playerCTRL.SetPlayerName(conn, numPlayers);
+            playerCTRL.TargetShowWelcomeMessage(conn, "You are the host! Lets hope you dont have hacks to find that treasure.");
+            
+        }
         else
         {
             // Any player after that
-             playerCTRL.TargetShowWelcomeMessage(conn, "You are a client. Get ready to find yourself some treasure!");
-         }
+            playerCTRL.SetPlayerName(conn, numPlayers);
+            playerCTRL.TargetShowWelcomeMessage(conn, "You are a client. Get ready to find yourself some treasure!");
+           
+        }
 
 
         /* 
@@ -49,10 +64,12 @@ public class NetworkManagerTemp : NetworkManager
     }
 
 
-    public void PlayerNewName(string name)
+  /*  public void PlayerNewName(string name)
     {
+        //player_Name = null;
+        //player_Names.Add(name);
         player_Name = name;
-    }
+    }*/
 }
 
    
