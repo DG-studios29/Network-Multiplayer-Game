8d94769615f03f1227140b70136ad4fe6c13075e
NTTFBS <2551187@students.wits.ac.za>
2025-05-07 18:36:49 -0400

    fixes

--- Network Multiplayer Game/Assets/Scripts/Island/Cannonball.cs
+++ Network Multiplayer Game/Assets/Scripts/Island/Cannonball.cs
@@ -8,7 +8,7 @@ public class Cannonball : NetworkBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!isServer) return;
+        //if (!isServer) return;
 
         if (collision.gameObject.CompareTag("Player"))
         {
